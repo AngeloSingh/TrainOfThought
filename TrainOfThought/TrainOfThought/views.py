fc69551e8efbea5786a635e9efd61047d2ff6530
Karl Hartmann <2566374h@student.gla.ac.uk>
2024-10-12 13:43:08 +0100

    Added function stubs

--- TrainOfThought/TrainOfThought/views.py
+++ TrainOfThought/TrainOfThought/views.py
@@ -1,6 +1,27 @@
-from django.http import Http404
+from django.http import JsonResponse
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, "TrainOfThought/index.html")
+
+def get_posts(request):
+    # TODO: Fetch posts
+    return JsonResponse(["test", "test"], safe=False)
+
+def evaluate_post(request):
+    # TODO: evaluates post passed in
+    return JsonResponse(["test", "test"], safe=False)
+
+def create_post(request):
+    # TODO: Gets user post and sends to database
+    return JsonResponse(["test", "test"], safe=False)
+
+def generate_post(request):
+    # TODO: ChatGPT post
+    return JsonResponse(["test", "test"], safe=False)
+
+def tick_posts(request):
+    # Fetch recent posts, then updates posts and users, and saves
+    # data = list(YourModel.objects.values())
+    return JsonResponse(["test", "test"], safe=False)
