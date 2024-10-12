eeb15ed445f60a9f2179d1df53102d88d4ea1585
interysy <2554268w@student.gla.ac.uk>
2024-10-12 21:45:21 +0100

    [200~feat(startpage): create startpage for app

--- TrainOfThought/backend/views.py
+++ TrainOfThought/backend/views.py
@@ -1,11 +1,12 @@
 from django.http import JsonResponse
-from .models import Bot, Post
+from .models import Bot, Creator, Post
 from django.http import JsonResponse
 from .models import Bot, Post
 from django.shortcuts import render
 from TrainOfThought.scripts.ai import gpt_post_response
 from rest_framework.decorators import api_view
 from django.views.decorators.csrf import csrf_protect
+from django.shortcuts import redirect
 
 def index(request):
     return render(request, "TrainOfThought/index.html")
@@ -17,7 +18,6 @@ def gpt_post(request):
         post = request.POST['user_input']
         person = request.POST['person']
         response = gpt_post_response(post, person)
-        #response = "This is a placeholder response"
         return render(request, "TrainOfThought/gpt-post.html", {'response': response})
     else:
         return render(request, "TrainOfThought/gpt-post.html")
@@ -37,12 +37,12 @@ def create_post(request):
     reposts (int): The number of reposts the post has
     '''
     post_data = request.data
-    
+
     try:
         bot = Bot.objects.get(id=post_data['bot'])
     except Bot.DoesNotExist:
         return JsonResponse({"error": "Bot does not exist"}, status=404)
-    
+
 
     data = {
         "bot": bot,
@@ -57,5 +57,45 @@ def create_post(request):
         post.save()
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=400)
-    
+
     return JsonResponse({"success": "Post created successfully"}, safe=False)
+
+
+
+def homepage(request):
+    creators = Creator.objects.all()
+    return render(request, 'TrainOfThought/homepage.html', {'creators': creators})
+
+
+
+@api_view(["POST"])
+@csrf_protect
+def select_creator(request):
+    if request.method == 'POST':
+        id = request.data['creatorId']
+
+        try:
+            bot = Bot.objects.get(id=id)
+
+            posts_to_delete = Post.objects.filter(bot=bot)
+            posts_to_delete.delete()
+
+
+            creator = Creator.objects.get(id=id)
+
+
+            my_bot = Bot.objects.get(id=0)
+            my_bot.reputation = creator.default_reputation
+            my_bot.hatred = creator.default_hatred
+            my_bot.popularity = creator.default_popularity
+            my_bot.networth = creator.networth
+
+            my_bot.save()
+
+            return JsonResponse({'message': 'Creator selected successfully', 'redirect_url': '/', 'success': True})
+
+        except Creator.DoesNotExist:
+            return JsonResponse({'message': 'Creator not found', 'success': False})
+
+
+    return JsonResponse({'message': 'Creator selected successfully', 'redirect_url': '/', 'success': False})
