58736af8cccf08e0c419c9a500cc8729436fee8a
interysy <2554268w@student.gla.ac.uk>
2024-10-12 13:10:57 +0100

    Initial Models

--- TrainOfThought/backend/admin.py
+++ TrainOfThought/backend/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Creator, Bot
 
 # Register your models here.
+
+admin.register(Creator)
+admin.register(Bot)
--- TrainOfThought/backend/models.py
+++ TrainOfThought/backend/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Creator(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length=30)
+    description = models.CharField(max_length=150)
+    default_reputation = models.FloatField()
+    default_hatred = models.FloatField()
+    default_likeness = models.FloatField()
+    default_popularity = models.FloatField()
+    networth = models.DecimalField(max_digits=15, decimal_places=2)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
+class Bot(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    reputation = models.FloatField()
+    hatred = models.FloatField()
+    likeness = models.FloatField()
+    popularity = models.FloatField()
+    networth = models.DecimalField(max_digits=15, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
