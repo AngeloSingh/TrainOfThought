ed345b149a49a8c9d2758db70cc4f20f79a6e23f
AngeloSingh <angelosingh32@gmail.com>
2024-10-12 11:02:15 +0100

    django setup

---
+++ TrainOfThought/TrainOfThought/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for TrainOfThought project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TrainOfThought.settings')
+
+application = get_asgi_application()
